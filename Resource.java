505a44d659755b52387b3d6c2e512dfbd3e87013
KonniHanka <82961397+KonniHanka@users.noreply.github.com>
2021-05-07 15:20:54 +0200

    Update Resource.java

--- Resource.java
+++ Resource.java
@@ -1,33 +1,23 @@
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 /**
  * Beschreiben Sie hier die Klasse Resource.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Emma Holtz und Karl Konrad Hanka
+ * @version Prototyp 1 7.5.2021
  */
 public class Resource
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Resource
-     */
-    public Resource()
+    public static BufferedImage getResourceImage(String name)
     {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+        BufferedImage img = null;
+        try{
+            img = ImageIO.read(new File("images/" + name));
+        } catch(IOException ex){
+            ex.printStackTrace();
+        }
+        return img;
     }
 }
